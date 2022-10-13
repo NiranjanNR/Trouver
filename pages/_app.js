9972b8476247d16c690c86fcc7add994631596ae
Uday Krishnan <udaykrishnanvg@gmail.com>
2022-10-13 23:32:35 +0530

    Added footer to the site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import '../styles/Page1.css'
 import '../components/NavBar/NavBar.css'
-
+import '../styles/foocss.css'
 
 
 function MyApp({ Component, pageProps }) {
