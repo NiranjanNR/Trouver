2482b92af874fac684d3f042c6a6c3c3308c5302
Uday Krishnan <udaykrishnanvg@gmail.com>
2022-11-13 21:18:24 +0530

    Added link-pages content

---
+++ pages/Aboutus.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Testnav from './Testnav'
+function Aboutus() {
+  return (
+    <div className='thanku h-[100vh]'>
+        <Testnav/>
+        <h1 className="text-center text-2xl mb-5 ml-5 text font-bold">The Team Behind This</h1>
+
+        <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 gap-2 ">
+        <div>The gif to be placed</div>
+    <div className='lg:p-20 p-10'>
+            <div className=' rounded-lg shadow-2xl border-4  border border-blue-400 border-opacity-50 hover:border-blue-500 p-20 mb-5'>
+            <h1 className="text-center text-xl mb-5 ml-5 text font-bold">SSR22-23 - 141</h1>
+            <div class="grid grid-cols-2 gap-2 ">
+               <p className='p-2 text-xl'>Advait Bal </p>
+               <p className='p-2 text-xl'>- AM.EN.U4CSE20104</p>
+               <p className='p-2 text-xl'>N R Niranjan  </p>
+               <p className='p-2 text-xl'>- AM.EN.U4CSE20249</p>
+               <p className='p-2 text-xl'>R Adithya Kumar</p>
+               <p className='p-2 text-xl'>- AM.EN.U4CSE20258</p>
+               <p className='p-2 text-xl'>V G Uday Krishnan</p>
+               <p className='p-2 text-xl'>- AM.EN.U4CSE20273</p>
+               <p className='p-2 text-xl'>Vishaal Sajeev Upot</p>
+               <p className='p-2 text-xl'>- AM.EN.U4CSE20276</p>
+              </div>
+              </div>
+              </div>
+              
+              </div>
+    </div>
+  )
+}
+
+export default Aboutus
---
+++ pages/Resmap.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Testnav from './Testnav'
+function Resmap() {
+  return (
+    <div className='thanku h-[100vh]'>
+        <Testnav/>
+        <h1 className="text-center text-2xl mb-5 ml-5 text font-bold">Mapping The Resources We Need</h1>
+
+        <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 gap-2 ">
+    <div className='lg:p-20 p-10'>
+            <div className=' rounded-lg shadow-2xl border-4  border border-blue-400 border-opacity-50 hover:border-blue-500 p-20 mb-5'>
+               <p className='p-1 text-xl'>● Implementation of the website is done using NextJS framework.</p>
+              <p className='p-1 text-xl'>● For designing the website, we will have to use several libraries like tailwindcss.</p>
+              <p className='p-1 text-xl'>● The website will have to be hosted on services like Heroku, Netlify, etc.</p>
+              <p className='p-1 text-xl'>● Requirement of transportation for collecting information from clinics, hospitals and medical shops.</p>
+              </div>
+              </div>
+              <div>The gif to be placed</div>
+              </div>
+    </div>
+  )
+}
+
+export default Resmap
